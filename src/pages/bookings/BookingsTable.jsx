c24c5f5389d2a1a6dec8de9f4aa79e5ee0fbae44
aritos20 <agianicopulos@gmail.com>
2023-04-18 19:01:27 +0200

    add a toast to handle an error if the api fetch fail

--- src/pages/bookings/BookingsTable.jsx
+++ src/pages/bookings/BookingsTable.jsx
@@ -53,7 +53,18 @@ const BookingsTable = () => {
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "light",
+          theme: "colored",
+          });
+      } else if (status === 'rejected') {
+        return toast.error('An error has ocurred, please try again', {
+          position: "top-center",
+          autoClose: 1500,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
           });
       }
     }
